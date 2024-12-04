9c8723f9f8c9b13debeeef2a9977002c28f997bf
Anil Paudel <gaindakot.anil@gmail.com>
2024-12-04 20:07:39 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Back to Top Button Functionality
+const backToTopButton = document.getElementById('back-to-top');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.addEventListener('click', function(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+// Back to Top Button Functionality
+const backToTopButton = document.getElementById('back-to-top');
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.addEventListener('click', function(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
