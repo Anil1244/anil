c366422affcbbf7de5d341a154768c04aed81463
Anil Paudel <gaindakot.anil@gmail.com>
2024-12-04 20:36:52 +0545

    Third commit

--- script.js
+++ script.js
@@ -12,17 +12,3 @@ window.onscroll = function() {
 backToTopButton.addEventListener('click', function(){
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
-// Back to Top Button Functionality
-const backToTopButton = document.getElementById('back-to-top');
-
-window.onscroll = function() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
-};
-
-backToTopButton.addEventListener('click', function(){
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
